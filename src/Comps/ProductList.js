f61d2f62f88653216fb60bf9950679589e44cc8c
ajb85 <ajbrush85@gmail.com>
2018-09-10 22:20:07 -0400

    modifyList renamed to productList.

--- src/Comps/ProductList.js
+++ src/Comps/ProductList.js
@@ -1,33 +1,55 @@
 import React, { Component } from "react";
 import TableRow from "./TableRow";
 
-class ModifyList extends Component {
+class ProductList extends Component {
   constructor(props) {
     super(props);
     this.onInputChange = this.onInputChange.bind(this);
     this.addRow = this.addRow.bind(this);
     this.deleteRow = this.deleteRow.bind(this);
-    this.consoleLog = this.consoleLog.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
     this.state = {
-      rows: this.props.currentList
+      rows: this.props.productList
     };
   }
   componentDidUpdate(prevProps, prevState) {
-    console.log(this.state.rows);
-    const stateDidChange = this.arrayOfObjectsDidChange(
-      this.state.rows,
-      prevState.rows
-    );
-    const dataWasChanged = this.arrayOfObjectsDidChange(
-      this.state.rows,
-      this.props.currentList
-    );
+    //Check to see if both the state was changed && if the data is different
+    //from props.  Both are needed to prevent an infinite loop.  If just
+    //state is checked, it'll see the state is different and refresh constantly
+    //if just the data is checked, it'll refresh infinitely since once the
+    //input differs from what's saved, dataWasChanged will always be true.
+    const stateDidChange = this.didInputChange(prevState.rows);
+    const dataWasChanged = this.didInputChange(this.props.productList);
     if (dataWasChanged && stateDidChange) {
       this.setState({ changed: true });
     } else if (stateDidChange && !dataWasChanged) {
       this.setState({ changed: false });
     }
+  }
+  //Returns "true" if the two arrays are different
+  //"false" if the content and order are the same
+  didInputChange(oldState) {
+    const newState = this.state.rows;
+    let changed = false;
+    newState.forEach(function(obj, i) {
+      //If oldState doesn't exist, then there's a new row
+      //in newState, therefore there's been a change,
+      // return true;
+      if (oldState[i]) {
+        //Check both objects to see if their key:value
+        //pairs are the same.
+        for (var key in obj) {
+          if (obj[key] !== oldState[i][key]) {
+            changed = true;
+          }
+        }
+      } else {
+        changed = true;
+      }
+    });
+    //If the order and all the values of the objects
+    //are the same, changed = false
+    return changed;
   }
   componentDidMount() {
     if (!this.state.rows.length) {
@@ -51,7 +73,6 @@ class ModifyList extends Component {
       ]
     });
   }
-
   addRow() {
     this.setState({
       rows: [
@@ -64,16 +85,12 @@ class ModifyList extends Component {
     let style = { fontStyle: "italic" };
     return (
       <tr onClick={this.addRow}>
-        <td colspan="5" className="ghostRow">
+        <td colSpan="5" className="ghostRow">
           ↓ <span style={style}>Add Row</span> ↓
         </td>
       </tr>
     );
   }
-  consoleLog() {
-    console.log(this.state.rows);
-  }
-
   onSubmit(event) {
     event.preventDefault();
     let updateList = this.props.updateList;
@@ -81,7 +98,7 @@ class ModifyList extends Component {
     const saveFileURL = "./saveInvData.php";
     var fetchData = {
       method: "POST",
-      body: JSON.stringify([this.state.rows, "modifyList"]),
+      body: JSON.stringify([this.state.rows, "productList"]),
       cache: "no-cache"
     };
     fetch(saveFileURL, fetchData)
@@ -92,31 +109,6 @@ class ModifyList extends Component {
       });
     return false;
   }
-  //Returns "true" if the two arrays are different
-  //"false" if the content and order are the same
-  arrayOfObjectsDidChange(newArr, oldArr) {
-    let changed = false;
-    newArr.forEach(function(obj, i) {
-      //If oldArr doesn't exist, then there's a new row
-      //in newArr, therefore there's been a change,
-      // return true;
-      if (oldArr[i]) {
-        //Check both objects to see if their key:value
-        //pairs are the same.
-        for (var key in obj) {
-          if (obj[key] !== oldArr[i][key]) {
-            changed = true;
-          }
-        }
-      } else {
-        changed = true;
-      }
-    });
-    //If the order and all the values of the objects
-    //are the same, changed = false
-    return changed;
-  }
-
   render() {
     let saveButton;
     if (this.state && this.state.changed) {
@@ -157,4 +149,4 @@ class ModifyList extends Component {
     );
   }
 }
-export default ModifyList;
+export default ProductList;
