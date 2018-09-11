70597b4e23aa41e69a54fa6eb1aaf69eed7affe7
ajb85 <ajbrush85@gmail.com>
2018-09-10 22:15:16 -0400

    Renamed variables for better readability, simplified some code

--- src/App.js
+++ src/App.js
@@ -3,64 +3,64 @@ import "./css/invmgr.css";
 import "./css/table.css";
 import InvHeader from "./Comps/InvHeader.js";
 import AppIndex from "./Comps/AppIndex.js";
-//import NewInventory from "./Comps/NewInventory.js";
-//import ModifyList from "./Comps/ModifyList.js";
-//import InputInvoice from "./Comps/InputInvoice.js";
-import Volume from "./Comps/Volume.js";
+//import Volume from "./Comps/Volume.js";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.updateListState = this.updateListState.bind(this);
-    this.fetchListData = this.fetchListData.bind(this);
+    this.updateStateCallback = this.updateStateCallback.bind(this);
+    this.fetchUserData = this.fetchUserData.bind(this);
     this.changePage = this.changePage.bind(this);
     this.state = {
-      page: <Volume />
+      page: <AppIndex />,
+      fetched: {}
     };
   }
   componentDidMount() {
-    this.fetchListData();
+    this.fetchUserData();
   }
-  updateListState(data) {
-    this.setState({ currentList: data });
+  updateStateCallback(state, data) {
+    const newFetched = this.state.fetched;
+    newFetched[state] = data;
+    this.setState({ fetched: newFetched });
   }
-  fetchListData() {
-    let callback = this.updateListState;
-    //Local testing code
-    callback(localData);
-    //Live server code
+  fetchUserData() {
+    let callback = this.updateStateCallback;
     /*
+    //Local testing code
+    callback("productList", localList);
+    callback("volumes", localVolume);
+    callback("inventories", localInventory);
+    */
+    //Live server code
     const saveFileURL = "./loadInvData.php";
-    var fetchData = {
-      method: "POST",
-      body: "modifyList",
-      cache: "no-cache"
-    };
-    fetch(saveFileURL, fetchData)
-      .then(res => res.json())
-      .then(data => callback(data));
-*/
+    const filesToFetch = ["productList", "volumes", "inventories"];
+    filesToFetch.forEach(function(file) {
+      const fetchData = {
+        method: "POST",
+        body: file,
+        cache: "no-cache"
+      };
+      fetch(saveFileURL, fetchData)
+        .then(res => res.json())
+        .then(data => callback(file, data));
+    });
+    //
   }
 
   changePage(newPage) {
     this.setState({ page: newPage });
   }
   render() {
-    let pageToLoad;
-    let header;
-    if (this.state && this.state.page) {
-      pageToLoad = this.state.page;
-    }
+    const pageToLoad = this.state.page;
+    const header = (
+      <InvHeader
+        loadNewPage={this.changePage}
+        fetched={this.state.fetched}
+        updateList={this.fetchUserData}
+      />
+    );
 
-    if (this.state && this.state.currentList) {
-      header = (
-        <InvHeader
-          callback={this.changePage}
-          currentList={this.state.currentList}
-          updateList={this.fetchListData}
-        />
-      );
-    }
     return (
       <div className="App">
         {header}
@@ -71,96 +71,150 @@ class App extends Component {
 }
 
 export default App;
-
-//Local test Code
-const localData = [
+/*
+//Local test Data
+const localVolume = {
+  "1545886800000": 85,
+  "1545973200000": 8,
+  "1546059600000": 74,
+  "1546232400000": 53,
+  "1546318800000": 36,
+  "1546405200000": 53,
+  "1546491600000": 85,
+  "1546578000000": 11,
+  "1546664400000": 42,
+  "1546750800000": 4,
+  "1546837200000": 28,
+  "1546923600000": 23,
+  "1547010000000": 38,
+  "1547096400000": 4,
+  "1547182800000": 88,
+  "1547269200000": 33,
+  "1547355600000": 43,
+  "1547442000000": 69,
+  "1547528400000": 92,
+  "1547614800000": 76
+};
+const localList = [
   {
-    item: "Planter's Peanuts",
+    item: "Doritos Cool Ranch",
     measurement: "bag",
-    upc: "18",
-    cost: "",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
   {
-    item: "Slim Jims",
-    measurement: "stick",
-    upc: "24",
-    cost: "",
+    item: "Doritos Nacho Cheese",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
   {
-    item: "Orbit Gum",
-    measurement: "pack",
-    upc: "12",
-    cost: "",
+    item: "Baked Lays",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
-  { item: "Mentos", measurement: "roll", upc: "15", cost: "", price: "1.87" },
   {
-    item: "Keebler Crackers",
-    measurement: "packet",
-    upc: "12",
-    cost: "",
-    price: "1.4"
+    item: "BBQ Lays",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
+    price: "1.87"
   },
   {
-    item: "Aviator Playing Cards",
-    measurement: "deck",
-    upc: "15",
-    cost: "",
-    price: "4.67"
+    item: "Sun Chips Harvest Chedder",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
+    price: "1.87"
   },
-  { item: "Ruffles", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Lays", measurement: "bag", upc: "1", cost: "", price: "1.4" },
   {
     item: "SmartFood Popcorn",
     measurement: "bag",
     upc: "1",
-    cost: "",
-    price: "1.4"
-  },
-  { item: "Sun Chips", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Doritos", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Gardetto's", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Pretzels", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Cheetos", measurement: "bag", upc: "1", cost: "", price: "1.4" },
-  { item: "Pop Tarts", measurement: "pack", upc: "6", cost: "", price: "1.87" },
-  { item: "Payday", measurement: "bar", upc: "", cost: "", price: "1.87" },
-  {
-    item: "Bufferfinger",
-    measurement: "bar",
-    upc: "36",
-    cost: "",
+    cost: ".65",
     price: "1.87"
   },
   {
-    item: "Starburst",
-    measurement: "pack",
-    upc: "36",
-    cost: "",
+    item: "Rold Gold Pretzels",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
-  { item: "Skittles", measurement: "bag", upc: "36", cost: "", price: "1.87" },
-  { item: "Twizzlers", measurement: "pack", upc: "", cost: "", price: "1.87" },
-  { item: "Milky Way", measurement: "bar", upc: "", cost: "", price: "1.87" },
   {
-    item: "Nature's Valley",
-    measurement: "bar",
-    upc: "16",
-    cost: "",
+    item: "Gardettos",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
-  { item: "Nutrigrain", measurement: "bar", upc: "16", cost: "", price: "1.4" },
-  { item: "Hershey's", measurement: "bar", upc: "36", cost: "", price: "1.87" },
-  { item: "Reese's", measurement: "pack", upc: "", cost: "", price: "1.87" },
-  { item: "Kit Kat", measurement: "bar", upc: "36", cost: "", price: "1.87" },
-  { item: "M&Ms", measurement: "pack", upc: "36", cost: "", price: "1.87" },
+  { item: "Ruffles", measurement: "bag", upc: "1", cost: ".65", price: "1.87" },
   {
-    item: "M&Ms Peanut",
-    measurement: "pack",
-    upc: "48",
-    cost: "",
+    item: "Ruffles Sour Cream/Chedder",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
     price: "1.87"
   },
-  { item: "Snickers", measurement: "bar", upc: "48", cost: "", price: "1.87" }
+  { item: "Cheetos", measurement: "bag", upc: "1", cost: ".65", price: "1.87" },
+  {
+    item: "Cheetos Flaming Hot",
+    measurement: "bag",
+    upc: "1",
+    cost: ".65",
+    price: "1.87"
+  },
+  { item: "Fritos", measurement: "bag", upc: "1", cost: ".65", price: "1.87" },
+  {
+    item: "Slim Jim Mild",
+    measurement: "stick",
+    upc: "24",
+    cost: "22.77",
+    price: "1.87"
+  },
+  {
+    item:
+      "ZXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    measurement: "stick",
+    upc: "24",
+    cost: "22.77",
+    price: "1.87"
+  }
 ];
+const localInventory = {
+  "1535083200000": {
+    "Doritos Cool Ranch": { unitCount: "6" },
+    "Doritos Nacho Cheese": { unitCount: "11" },
+    "Baked Lays": { unitCount: "12" },
+    "BBQ Lays": { unitCount: "5" },
+    "Sun Chips Harvest Chedder": { unitCount: "32" },
+    "SmartFood Popcorn": { unitCount: "2", caseCount: "" },
+    "Rold Gold Pretzels": { unitCount: "4" },
+    Gardettos: { unitCount: "5" },
+    Ruffles: { unitCount: "10" },
+    "Ruffles Sour Cream/Chedder": { unitCount: "32" },
+    Cheetos: { unitCount: "1" },
+    "Cheetos Flaming Hot": { unitCount: "14" },
+    Fritos: { unitCount: "13" }
+  },
+  "1534478400000": {
+    "Doritos Cool Ranch": { unitCount: "6" },
+    "Doritos Nacho Cheese": { unitCount: "11" },
+    "Baked Lays": { unitCount: "12" },
+    "BBQ Lays": { unitCount: "5" },
+    "Sun Chips Harvest Chedder": { unitCount: "32" },
+    "SmartFood Popcorn": { unitCount: "2", caseCount: "" },
+    "Rold Gold Pretzels": { unitCount: "4" },
+    Gardettos: { unitCount: "5" },
+    Ruffles: { unitCount: "10" },
+    "Ruffles Sour Cream/Chedder": { unitCount: "32" },
+    Cheetos: { unitCount: "1" },
+    "Cheetos Flaming Hot": { unitCount: "14" },
+    Fritos: { unitCount: "13" }
+  }
+};
+*/
